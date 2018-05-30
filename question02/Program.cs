92af9f705199a15874590476659ac149bd6be917
singha37 <singha37@g.toiohomai.ac.nz>
2018-05-31 09:59:44 +1200

    Q2-L14,there is a incomplete sentence Write changed into WriteLine

--- question02/Program.cs
+++ question02/Program.cs
@@ -11,7 +11,7 @@ namespace question02
             string[] fruits = new string[4] {"Apple", "Banana", "Coconut", "Mandarin"};
 
             foreach(var fruit in fruits) {
-                Console.Write(fruit);
+                Console.WriteLine(fruit);
             }
         }
     }
