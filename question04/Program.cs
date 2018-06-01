09d1a697fb239e562aacbfeaa13310973fa76590
singha37 <singha37@g.toiohomai.ac.nz>
2018-06-01 13:18:24 +1200

    Q4-L11- I used there Convert.ToInt32 because Converts the value of the specified 16-bit unsigned integer to the equivalent 32-bit signed integer

--- question04/Program.cs
+++ question04/Program.cs
@@ -8,7 +8,7 @@ namespace question04
         {
             Console.WriteLine("Print out the reverse timestables (highest number first) for:");
 
-            var number = Console.ReadLine();
+            var number = Convert.ToInt32(Console.ReadLine());
 
             for(var i = 0; i < 12; i++) {
                 var a = i + 1;
@@ -17,3 +17,4 @@ namespace question04
         }
     }
 }
+)
